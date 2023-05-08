e9f7036755ed6f6ebc503b4c60a9a8c38bc2e8c5
ElieBadari <elie.badari@hotmail.com>
2023-05-08 19:58:37 +0300

    converted to offerings to csv

--- lau-scrape.py
+++ lau-scrape.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from getpass import getpass
 from selenium.webdriver.support.ui import Select
+import csv
 
 
 def getLogin():
@@ -46,9 +47,34 @@ def extractToCSV(offerings):
     rows = list()
     for row in table.findAll("tr"):
         rows.append(row)
-    
-    
+    with open("output.csv", "w") as output_file:
+        writer = csv.writer(output_file)
+        headers = rows[1]
+        header_cells = list()
+        header_cells = [cell.string.strip() if cell.string else i.string.strip() for cell in headers.findAll(["td", "th"]) for i in cell.findAll()]
+        writer.writerow([cell for i, cell in enumerate(header_cells) if i != 0 and i < 20])
+        
+        for i in range(2, len(rows)):
+            cells = list()
+            row = rows[i]
+            for cell in row.findAll(["td", "tr"]):
+                if row.findAll(["td", "tr"]).index(cell) != 0 and row.findAll(["td", "tr"]).index(
+                        cell) < 20:
+                    if cell.string:
+                        cells.append(cell.string.strip())
+                    else:
+                        children = cell.findAll()
+                        for i in children:
+                            cells.append(i.string.strip())
+            if len(cells) > 4:
+                if eval(cells[4]) == 2:
+                    writer.writerow(cells)
+            else:
+                writer.writerow(cells)
+
+        
 
 login_info = getLogin()
 course_offerings = getCourseOfferings(login_info)
+extractToCSV(course_offerings)
 
