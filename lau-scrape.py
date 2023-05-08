1f3318a70438b961dab567925efbdfc05a11d849
ElieBadari <elie.badari@hotmail.com>
2023-05-08 18:59:53 +0300

    automated sending login info

--- lau-scrape.py
+++ lau-scrape.py
@@ -1,4 +1,4 @@
-
+from selenium import webdriver as wd
 
 def getLogin():
     login_info = {}
@@ -8,3 +8,9 @@ def getLogin():
     login_info["password"] = password
     return login_info
 
+def getCourseOfferings(login_info):
+    chrome = wd.Chrome()
+    chrome.get("https://banweb.lau.edu.lb/")
+    chrome.find_element(by="id",value="username").send_keys(login_info["username"])
+    chrome.find_element(by="id",value="password").send_keys(login_info["password"])
+    chrome.find_element(by="css selector",value='[type="submit"]').click()
